914e5755c80800c081e1a00ef4de53840fbec4b7
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-04-08 15:31:33 +0100

    has a minimum temp, temp wont decrease below minimum
    Co-Authored-By: willspencer16 <77449895+willspencer16@users.noreply.github.com>

--- jasmine/jasmine-standalone-3.7.1/spec/thermostatSpec.js
+++ jasmine/jasmine-standalone-3.7.1/spec/thermostatSpec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 describe('Thermostat', function() {
   let thermostat
 
@@ -9,15 +11,40 @@ describe('Thermostat', function() {
     it('of 20 degrees', function(){
       expect(thermostat.startTemp).toBe(20);
     });
-    
+  });
+
+  describe('has a minimum temperature', function() {
+    it('of 10 degrees', function(){
+      expect(thermostat.MINIMUMTEMP).toBe(10);
+    });
+
+    it('that is true or false', function(){
+      for(var i=0; i<10; i++){
+        thermostat.down();
+      }
+      expect(thermostat.isMinimumTemperature()).toBe(true);
+    });
+
+
+    it('that it will not go below', function(){
+      for(var i=0; i<11; i++){
+        thermostat.down();
+      }
+      expect(thermostat.temp).toBe(10);
+    });
   });
 
   describe('has a temperature', function() {
     it('that can be increased', function(){
-      thermostat.up(1)
+      thermostat.up()
       expect(thermostat.temp).toBe(21);
     });
-    
+
+    it('that can be decreased', function(){
+      thermostat.down()
+      expect(thermostat.temp).toBe(19);
+    });
+
   });
 
 });
--- jasmine/jasmine-standalone-3.7.1/src/thermostat.js
+++ jasmine/jasmine-standalone-3.7.1/src/thermostat.js
@@ -1,10 +1,24 @@
+'use strict';
+
 class Thermostat {
   constructor(){
     this.startTemp = 20;
     this.temp = 20;
+    this.MINIMUMTEMP = 10;
   }
 
-  up(number){
-  	return this.temp += number;
+  up(){
+  	return this.temp += 1;
   }
+
+  down(){
+    if (this.isMinimumTemperature()){
+      return;
+    }
+     this.temp -= 1;
+    }
+
+  isMinimumTemperature() {
+   return this.temp === this.MINIMUMTEMP;
+ }
 };
