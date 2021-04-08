38f43f9fa4458cf9bf2d05008c420ff3aa1dad0e
Will Spencer <willspencer16@gmail.com>
2021-04-08 14:46:52 +0100

    Added function to increase the temperature
    Co-authored-by: Lou <lou.m.reade@gmail.com>

--- jasmine/jasmine-standalone-3.7.1/spec/thermostatSpec.js
+++ jasmine/jasmine-standalone-3.7.1/spec/thermostatSpec.js
@@ -1,10 +1,21 @@
 describe('Thermostat', function() {
   let thermostat
 
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
   describe('has a start temperature', function() {
     it('of 20 degrees', function(){
-      thermostat = new Thermostat();
       expect(thermostat.startTemp).toBe(20);
+    });
+    
+  });
+
+  describe('has a temperature', function() {
+    it('that can be increased', function(){
+      thermostat.up(1)
+      expect(thermostat.temp).toBe(21);
     });
     
   });
--- jasmine/jasmine-standalone-3.7.1/src/thermostat.js
+++ jasmine/jasmine-standalone-3.7.1/src/thermostat.js
@@ -1,5 +1,10 @@
 class Thermostat {
   constructor(){
     this.startTemp = 20;
+    this.temp = 20;
+  }
+
+  up(number){
+  	return this.temp += number;
   }
 };
