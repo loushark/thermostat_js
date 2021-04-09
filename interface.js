4983758921ea29fbca320bcda0f3cbdc97f46b25
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-04-09 17:04:41 +0100

    jquery update powersavingmode, leds, and energy usage buttons
    Co-Authored-By: Alec Yates <56926715+Yates101@users.noreply.github.com>

--- interface.js
+++ interface.js
@@ -3,15 +3,52 @@ $( document ).ready(function( ) {
   var thermostat = new Thermostat();
 
   updateTemp();
-  $("#powerMode").text("Power mode is: ");
-  $("#energy").text("The energy usage is: ");
+  updatePowerMode();
+  displayEnergyUsage()
 
   $( "#changeTempUp" ).click(function() {
     thermostat.up();
     updateTemp();
   });
 
+  $( "#changeTempDown" ).click(function() {
+    thermostat.down();
+    updateTemp();
+  });
+
+  $( "#powerModeOn" ).click(function() {
+    thermostat.powerSavingModeOn();
+    updatePowerMode();
+  });
+
+  $( "#powerModeOff" ).click(function() {
+    thermostat.powerSavingModeOff();
+    updatePowerMode();
+  });
+
+  $( "#energyUsage" ).click(function() {
+    thermostat.energyUsage();
+    displayEnergyUsage();
+  });
+
+
+
   function updateTemp() {
     $("#changeTemp").text("Current Temp is: " + thermostat.temp);
   }
+
+  function updatePowerMode() {
+    if(thermostat.powerSavingMode === true){
+      $("#powerMode").text("Power saving mode is: on");
+      $("#PSMLight").attr("class", "led-green");
+    } else {
+    $("#powerMode").text("Power saving mode is: off ");
+    $("#PSMLight").attr("class", "led-red");
+    }
+  }
+
+  function displayEnergyUsage() {
+    $("#energy").text("The energy usage is: " + thermostat.energyUsage());
+  }
+
 });
